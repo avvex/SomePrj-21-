0fb17d3ba7a78c32898daf7cf10002f11fc0ce61
Valentina Andreeva <50757293+avvex@users.noreply.github.com>
2022-02-25 15:20:18 +0700

    AddMain

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <QCoreApplication>
+class A
+{
+
+public:
+    int X;
+};
+
+class B
+{
+
+public:
+    int X;
+};
+
+class Steck
+{
+
+public:
+    int X;
+};
+
+class SteckA
+{
+
+public:
+    int X;
+};
+
+class SteckABNew
+{
+
+public:
+    int X;
+};
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    return      1; //a.exec();
+}
